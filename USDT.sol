85a096bdd7b47a8fad754898387cdd2a144b91af
Henrique Moreira <henriquemoreira842@gmail.com>
2025-11-18 19:10:35 -0300

    Create USDT.sol

---
+++ USDT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/*
+    Jeftar Mascarenhas
+    twitter: @jeftar
+    github: github.com/jeftarmascarenhas
+    linkedin: linkedin.com/in/jeftarmascarenhas/
+    site: jeftar.com.br
+    youtube: youtube.com/@nftchoose
+*/
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Token de Recompensa - Reward Token
+contract USDT is ERC20, Ownable {
+    constructor() ERC20("USDT",  "USDT") Ownable(msg.sender) {}
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+}
